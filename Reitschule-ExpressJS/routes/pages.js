643c6a5a3c722873731cea7478f4ff361b2ba21f
cxs1mon <caroline.simon@gmx.ch>
2024-12-02 16:49:00 +0100

    added routes for each page

--- Reitschule-ExpressJS/routes/pages.js
+++ Reitschule-ExpressJS/routes/pages.js
@@ -1,6 +1,14 @@
 const { Router } = require("express");
 const allRoutes = Router();
 
-allRoutes.get("/", (req, res) => res.render('index')); // No need for extension or relative path
+allRoutes.get("/(index)?(.html)?", (req, res) => res.render('index'));
+
+allRoutes.get("/angebot(.html)?", (req, res) => res.render('angebot'));
+
+allRoutes.get("/meine-pferde(.html)?", (req, res) => res.render('meine-pferde'));
+
+allRoutes.get("/kontakt(.html)?", (req, res) => res.render('kontakt'));
+
+allRoutes.get("/ueber-mich(.html)?", (req, res) => res.render('ueber-mich'));
 
 module.exports = allRoutes;
