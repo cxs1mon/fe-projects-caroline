31c3adb7c4174473e5d8f43e71e3e3e8e62bd5e0
cxs1mon <caroline.simon@gmx.ch>
2024-12-10 16:37:37 +0100

    add prominent class to h3 based on attributes

--- Reitschule-Components/public/js/components/text-block.js
+++ Reitschule-Components/public/js/components/text-block.js
@@ -3,17 +3,27 @@ class RTSTextBlock extends HTMLElement {
     connectedCallback() {
         const title = this.getAttribute('title');
         const text = this.getAttribute('text');
+        let h3Element;
+        
+        if (this.getAttribute('prominent')) {
+            const prominent = this.getAttribute('prominent');
+            console.log("prominent");
+            h3Element = `<h3 class="text-block__subtitle text-block__subtitle--prominent">${title}</h3>`
+        } else {
+            console.log("not prominent")
+            h3Element = `<h3 class="text-block__subtitle">${title}</h3>`
+        }
         
         this.innerHTML =
         `
         <section class="text-block">
-            <h3 class="text-block__subtitle">${title}</h3>
+            ${h3Element}    
             <p class="text-block__text">
                 ${text}
             </p>
         </section>
         `;
-    }
+    }   
 }
 
 customElements.define("rts-text-block", RTSTextBlock);
