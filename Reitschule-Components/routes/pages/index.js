99f5462e9d7621dadbf331b3c484353c28ad5226
Samir Mumic <sam.mumic@gmail.com>
2024-12-12 10:41:38 +0100

    feat: adds working index js ejs file rendering

--- Reitschule-Components/routes/pages/index.js
+++ Reitschule-Components/routes/pages/index.js
@@ -1,9 +1,16 @@
-const { Router } = require("express");
+const {Router} = require("express");
 const indexRouter = Router();
-const { meineLeidenschaft, meinZiel } = require('../../config.js');
+const {meineLeidenschaft, meinZiel} = require('../../config.js');
+const ejs = require('ejs');
 
-indexRouter.get('/(index)?', (req, res) => {
-    res.render('layout', { meineLeidenschaft, meinZiel, body: "<%- include('views/page-content/index-content.ejs') %>" });
+indexRouter.get('/(index)?', async (req, res) => {
+    try {
+        const body = await ejs.renderFile('views/page-content/index-content.ejs', {meineLeidenschaft, meinZiel});
+        res.render('layout', {title: 'Willkommen', body});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error rendering the page");
+    }
 });
 
 module.exports = indexRouter;
