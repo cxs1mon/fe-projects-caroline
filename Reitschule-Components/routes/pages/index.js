0dd0d5564c1ee15a8f6419d980385f7ca9a3d18f
cxs1mon <caroline.simon@gmx.ch>
2024-12-18 08:20:01 +0100

    create layout-component and use slot for content

--- Reitschule-Components/routes/pages/index.js
+++ Reitschule-Components/routes/pages/index.js
@@ -1,16 +1,10 @@
-const {Router} = require("express");
+const { Router } = require('express');
 const indexRouter = Router();
-const {meineLeidenschaft, meinZiel} = require('../../config.js');
+const { meineLeidenschaft, meinZiel } = require('../../config.js');
 const ejs = require('ejs');
 
-indexRouter.get('/(index)?', async (req, res) => {
-    try {
-        const body = await ejs.renderFile('views/page-content/index-content.ejs', {meineLeidenschaft, meinZiel});
-        res.render('layout', {title: 'Willkommen', body});
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Error rendering the page");
-    }
+indexRouter.get('/', async (req, res) => {
+    res.render('index-content', {meineLeidenschaft, meinZiel, title: 'Willkommen'});
 });
 
 module.exports = indexRouter;
