9ec2cfd7c33dfc2b150d8651139b55f381cae6e2
cxs1mon <caroline.simon@gmx.ch>
2025-01-08 10:19:26 +0100

    working on bootstrap header

--- Reitschule-Bootstrap/public/js/components/nav.js
+++ Reitschule-Bootstrap/public/js/components/nav.js
@@ -4,14 +4,26 @@ class RTSNav extends HTMLElement {
         console.log(`Navigation was loaded on page ${window.location.href}`)
         this.innerHTML =
         `
-        <nav class="nav">
-            <ul class="nav__list">
-                <li class="nav__item "><a class="link nav__link" href="/">Home</a></li>
-                <li class="nav__item "><a class="link nav__link" href="angebot">Angebot</a></li>
-                <li class="nav__item "><a class="link nav__link" href="meine-pferde">Meine Pferde</a></li>
-                <li class="nav__item "><a class="link nav__link" href="ueber-mich">Über mich</a></li>
-                <li class="nav__item "><a class="link nav__link" href="kontakt">Kontakt</a></li>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Fixed navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
             </ul>
+            </div>
+        </div>
         </nav>
         `;
     }
