d186c5c5724438512a8ca0b713ef7d39d6952e42
cxs1mon <caroline.simon@gmx.ch>
2025-01-14 09:45:23 +0100

    rename "toggle-block-component" -> "horse-toggle-block-component"

--- Reitschule-Bootstrap/public/js/components/horse-toggle-block-component.js
+++ Reitschule-Bootstrap/public/js/components/horse-toggle-block-component.js
@@ -17,11 +17,11 @@ class RTSToggleText extends HTMLElement {
             <h5 class="summaryTitle">Mehr über ${name}</h5>
             <p class="summaryText">${text}</p>
             <ul class="list list--no-bullets">
-                <li class="list__item">Name: ${name}</li>
-                <li class="list__item">Rasse: ${breed}</li>
-                <li class="list__item">Geburtsjahr: ${birthyear}</li>
-                <li class="list__item">Farbe: ${color}</li>
-                <li class="list__item">Geschlecht: ${gender}</li>
+                <li class="list__item"><b>Name:</b> ${name}</li>
+                <li class="list__item"><b>Rasse:</b> ${breed}</li>
+                <li class="list__item"><b>Geburtsjahr:</b> ${birthyear}</li>
+                <li class="list__item"><b>Farbe:</b> ${color}</li>
+                <li class="list__item"><b>Geschlecht:</b> ${gender}</li>
             </ul>
             `
         } else {
