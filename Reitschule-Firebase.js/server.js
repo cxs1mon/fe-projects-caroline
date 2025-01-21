2d4fe91dd713ade255e8037bfc9f68f56783512b
cxs1mon <caroline.simon@gmx.ch>
2025-01-21 07:57:19 +0100

    delete all horses function

--- Reitschule-Firebase.js/server.js
+++ Reitschule-Firebase.js/server.js
@@ -1,51 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { db } = require('./config/firebase');
+const { db } = require("./config/firebase");
 const port = process.env.PORT || 8383;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.use(express.static(`public`));
 
-
 // Get all horses
-app.get('/', async (req, res) => {
+app.get("/", async (req, res) => {
   try {
-    const horsesSnapshot = (await db.collection('stable').get());
+    const horsesSnapshot = await db.collection("stable").get();
     const horses = [];
-    horsesSnapshot.forEach(doc => {
+    horsesSnapshot.forEach((doc) => {
       horses.push({ id: doc.id, ...doc.data() });
-      console.log(horses)
+      console.log(horses);
     });
-    res.render('horses', { horses });
+    res.render("horses", { horses });
   } catch (error) {
     res.status(500).send(error.message);
-    console.error("horses db not loaded")
+    console.error("horses db not loaded");
     console.error(error);
-    
   }
 });
 
 // Add new horse
-app.post('/api/add', async (req, res) => {
-  const {  name, age, color, breed } = req.body;
+app.post("/api/add", async (req, res) => {
+  const { name, age, color, breed } = req.body;
 
   if (!name || !age || !color || !breed) {
-    return res.status(400).send('Check your input, something is missing.');
+    return res.status(400).send("Check your input, something is missing.");
   }
   try {
-    await db.collection('stable').add({
+    await db.collection("stable").add({
       name,
       age,
       color,
       breed,
     });
-    res.status(201).send( 'New horse added!');
+    res.status(201).send("New horse added!");
   } catch (error) {
-    console.error('Error while adding horse.', error);
-    res.status(500).send('Error while adding horse.');
+    console.error("Error while adding horse.", error);
+    res.status(500).send("Error while adding horse.");
   }
 });
 
@@ -53,21 +51,33 @@ app.post('/api/add', async (req, res) => {
 app.post(`/api/delete/:id`, async (req, res) => {
   const horseId = req.params.id;
   try {
-    await db.collection('stable').doc(horseId).delete();
-    res.redirect('/');
+    await db.collection("stable").doc(horseId).delete();
+    res.redirect("/");
   } catch (error) {
     console.error("Error deleting horse: ", error);
-    res.status(500).send('Failed to delete horse.');
+    res.status(500).send("Failed to delete horse.");
   }
 });
 // TODO: Update horse
-app.post('/api/update/:id', async (req, res) => {
-
-});
+app.post("/api/update/:id", async (req, res) => {});
 
 // TODO: Delete all horses
-app.post('/api/delete-all', async (req, res) => {
+app.post("/api/delete-all", async (req, res) => {
+  try {
+    // get all documents
+    const val = await db.collection('stable').listDocuments();
+    
+    // for each document, delete it
+    for (let i = 0; i < val.length; i++) {
+      const document = val[i];
+      await document.delete();
+    }
 
+    res.redirect('/');
+  } catch (error) {
+    console.error("Error deleting all horses: ", error);
+    res.status(500).send('Failed to delete all horses.');
+  }
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
