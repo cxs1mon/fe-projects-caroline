a7a5eede9a316b128f97b795b02a2eb236bf5048
cxs1mon <caroline.simon@gmx.ch>
2025-01-21 13:08:23 +0100

    copy Firebase-Bootstrap and renamed it to Reitschule-Firebase-frontend

---
+++ Reitschule-Firebase-frontend/public/js/components/footer-component.js
@@ -0,0 +1,14 @@
+class RTSFooter extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <div class="container">
+            <footer class="py-3 my-4 border-top pt-3">
+                <p class="  text-center text-body-secondary">&copy; 2025 Caroline Simon - <a href="mailto:caroline.simon@gmx.ch" target="" class="link--underline link text-body-secondary">caroline.simon@gmx.ch</a></p>
+                <p class="text-center text-body-secondary">Made with <a href="https://getbootstrap.com/" class="link text-body-secondary link--underline">Bootstrap</a> examples</p>
+            </footer>
+        </div>
+        `;
+  }
+}
+
+customElements.define("rts-footer", RTSFooter);
