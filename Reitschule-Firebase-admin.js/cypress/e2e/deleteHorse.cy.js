1bcdcb99adbde8c99f10a7b29fd96fc5b99be899
cxs1mon <caroline.simon@gmx.ch>
2025-01-31 15:21:20 +0100

    wrote delete horse functionality test

--- Reitschule-Firebase-admin.js/cypress/e2e/deleteHorse.cy.js
+++ Reitschule-Firebase-admin.js/cypress/e2e/deleteHorse.cy.js
@@ -1,17 +1,26 @@
 describe("Delete Horse Tests", () => {
+  // Fixture einmal laden und die Daten speichern
+  let horseData;
+  before(() => {
+    cy.fixture("example.json").then((data) => {
+      horseData = data;
+    });
+  });
+
   beforeEach(() => {
     // Alle Pferde löschen
     cy.request("POST", "http://localhost:8383/api/delete-all");
 
     // Pferde aus der Fixture-Datei hinzufügen
-    cy.fixture("example.json").then((horseData) => {
-      cy.request("POST", "http://localhost:8383/api/add", horseData.horses[0]);
-      cy.request("POST", "http://localhost:8383/api/add", horseData.horses[1]);
-    });
+    cy.request("POST", "http://localhost:8383/api/add", horseData.horses[0]);
 
     cy.visit("http://localhost:8383/");
   });
-  it("should show delete confirmation dialog", () => {});
-  it("should successfully delete a horse", () => {});
-  it("should update the list after deletion", () => {});
+
+  it("should successfully delete a horse", () => {
+    cy.get("table").should("contain", horseData.horses[0].name);
+    cy.get(".delete-btn").click();
+    cy.reload();
+    cy.get("table").should("not.contain", horseData.horses[0].name);
+  });
 });
