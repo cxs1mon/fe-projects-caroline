2a19f0d64ac782d8935ea538b6097d7cece17349
cxs1mon <caroline.simon@gmx.ch>
2025-03-12 13:19:03 +0100

    new folder structure

--- JS-Quiz-react/src/components/pages/APIStartPage.js
+++ JS-Quiz-react/src/components/pages/APIStartPage.js
@@ -1,6 +1,5 @@
 import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./APIStartPage.css";
 import  Header from "./Header.js";
 
 export default function StartPage() {
